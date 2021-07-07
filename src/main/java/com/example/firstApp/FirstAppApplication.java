c3130f1a95539b586d185973a9636c03b7d15844
ragavanRagav <ragavan111999@gmail.com>
2021-07-07 18:05:36 +0530

    First commit

---
+++ src/main/java/com/example/firstApp/FirstAppApplication.java
@@ -0,0 +1,24 @@
+package com.example.firstApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class FirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAppApplication.class, args);
+		
+		System.out.println("Hello world");
+	}
+	
+	@GetMapping("/")
+	public String hello(@RequestParam(value = "name", defaultValue = "Ragavan") String name) {
+		return "welcome "+name;
+	}
+
+}
